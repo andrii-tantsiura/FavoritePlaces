48b0b908cae700e4a73c8d7da82c6aef07389920
andrii.tantsiura <andrii.tantsiura@headworks.com.ua>
2023-03-29 22:04:49 +0300

    Added ImagePicker android & ios

---
+++ components/Places/ImagePicker.js
@@ -0,0 +1,55 @@
+import { Alert, Button, View } from "react-native";
+import {
+  launchCameraAsync,
+  useCameraPermissions,
+  PermissionStatus,
+} from "expo-image-picker";
+
+function ImagePicker() {
+  const [cameraPermissionInformation, requestPermission] =
+    useCameraPermissions();
+
+  async function verifyPermissions() {
+    if (cameraPermissionInformation.status === PermissionStatus.UNDETERMINED) {
+      const response = await requestPermission();
+
+      return response.granted;
+    }
+
+    if (cameraPermissionInformation.status === PermissionStatus.DENIED) {
+      Alert.alert(
+        "Insufficient Permissions!",
+        "You need to grant camera permissions to user this app."
+      );
+
+      return false;
+    }
+
+    return true;
+  }
+
+  async function takeImageHandler() {
+    const hasPermission = await verifyPermissions();
+
+    if (!hasPermission) {
+      return;
+    }
+
+    const image = await launchCameraAsync({
+      allowsEditing: true,
+      aspect: [16, 9],
+      quality: 0.5,
+    });
+
+    console.log(image);
+  }
+
+  return (
+    <View>
+      <View></View>
+      <Button title="Take image" onPress={takeImageHandler} />
+    </View>
+  );
+}
+
+export default ImagePicker;
